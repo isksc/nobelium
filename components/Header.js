91f9d41b18e66d61eff03c16cb8ccd5678c30d8a
isksc <105981553+isksc@users.noreply.github.com>
2022-05-22 23:17:13 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -93,7 +93,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
                       gradientTransform="rotate(45) scale(39.598)"
                     >
                       <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
+                      <stop offset="1" stopColor="#fbb217" stopOpacity="0" />
                     </radialGradient>
                   </defs>
                 </svg>
