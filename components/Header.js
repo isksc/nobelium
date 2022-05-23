92fb5dc6ccb9441ced908bdb56cf62289eaf0980
isksc <105981553+isksc@users.noreply.github.com>
2022-05-23 16:26:43 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import { IoSunnyOutline, IoMoonSharp } from "react-icons/io5";
-import { useTheme } from "next-themes";
-import BLOG from "@/blog.config";
-import { useLocale } from "@/lib/locale";
+import { useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
+import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
+import { useTheme } from 'next-themes'
+import BLOG from '@/blog.config'
+import { useLocale } from '@/lib/locale'
 
 const NavBar = () => {
-  const locale = useLocale();
+  const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || "/", show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: "/about", show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.SEARCH, to: "/search", show: true },
-    { id: 3, name: locale.NAV.Navigation, to: "/Navigation", show: true },
-    { id: 4, name: locale.NAV.RSS, to: "/feed", show: true },
-  ];
+    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
+    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true },
+    { id: 3, name: locale.NAV.Navigation, to: '/Navigation', show: true },
+    { id: 4, name: locale.NAV.RSS, to: '/feed', show: true }
+  ]
   return (
     <div className="flex-shrink-0">
       <ul className="flex flex-row">
         {links.map(
-          (link) =>
+          link =>
             link.show && (
               <li
                 key={link.id}
@@ -32,38 +32,38 @@ const NavBar = () => {
         )}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 const Header = ({ navBarTitle, fullWidth }) => {
-  const useSticky = !BLOG.autoCollapsedNavBar;
-  const navRef = useRef(null);
-  const sentinalRef = useRef([]);
-  const { theme, setTheme } = useTheme();
-  const [hasMounted, setHasMounted] = useState(false);
+  const useSticky = !BLOG.autoCollapsedNavBar
+  const navRef = useRef(null)
+  const sentinalRef = useRef([])
+  const { theme, setTheme } = useTheme()
+  const [hasMounted, setHasMounted] = useState(false)
   useEffect(() => {
-    setHasMounted(true);
-  }, []);
+    setHasMounted(true)
+  }, [])
   const handler = ([entry]) => {
     if (navRef && navRef.current && useSticky) {
       if (!entry.isIntersecting && entry !== undefined) {
-        navRef.current?.classList.add("sticky-nav-full");
+        navRef.current?.classList.add('sticky-nav-full')
       } else {
-        navRef.current?.classList.remove("sticky-nav-full");
+        navRef.current?.classList.remove('sticky-nav-full')
       }
     } else {
-      navRef.current?.classList.add("remove-sticky");
+      navRef.current?.classList.add('remove-sticky')
     }
-  };
+  }
   useEffect(() => {
-    const obvserver = new window.IntersectionObserver(handler);
-    obvserver.observe(sentinalRef.current);
+    const obvserver = new window.IntersectionObserver(handler)
+    obvserver.observe(sentinalRef.current)
     // Don't touch this, I have no idea how it works XD
     // return () => {
     //   if (sentinalRef.current) obvserver.unobserve(sentinalRef.current)
     // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sentinalRef]);
+  }, [sentinalRef])
   return (
     <>
       <div className="observer-element h-4 md:h-12" ref={sentinalRef}></div>
@@ -97,11 +97,11 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <NavBar />
         <div>
           <a
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
             title={`Toggle dark mode - current ${theme}`}
-            className="hover:text-blue-400 cursor-pointer text-xl"
+            className='hover:text-blue-400 cursor-pointer text-xl'
           >
-            {hasMounted && theme === "dark" ? (
+            {hasMounted && theme === 'dark' ? (
               <IoMoonSharp />
             ) : (
               <IoSunnyOutline />
@@ -111,6 +111,6 @@ const Header = ({ navBarTitle, fullWidth }) => {
       </div>
     </>
   );
-};
+}
 
-export default Header;
+export default Header
